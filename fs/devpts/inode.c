47353958e46a19f260fd93c67b65cab885fddb8b
JJ-Coffee <akuntrial042@gmail.com>
2025-01-09 17:29:14 +0000

    update

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -606,7 +606,6 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 	return dentry;
 }
 
-
 #ifdef CONFIG_KSU
 extern int ksu_handle_devpts(struct inode*);
 #endif
@@ -622,6 +621,7 @@ void *devpts_get_priv(struct dentry *dentry)
 #ifdef CONFIG_KSU
  	ksu_handle_devpts(dentry->d_inode);
 #endif
+
 #if defined(CONFIG_KSU_SUSFS_SUS_SU)
 	if (ksu_devpts_hook) {
 		ksu_handle_devpts(dentry->d_inode);
